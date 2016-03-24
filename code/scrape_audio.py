07dff714c0c669c951caff2d2bc76b69523eac54
cferko <cferko@domeyard.com>
2016-03-23 22:21:50 -0400

    REV: fixing audio script, adding back files

--- code/scrape_audio.py
+++ code/scrape_audio.py
@@ -1,19 +1,18 @@
-from urllib2 import urlopen
+import requests
+from bs4 import BeautifulSoup
+import urllib
 
-urlpath =urlopen('http://www.hazmatt.net/gaming/starcraft/zerg/units/')
-string = urlpath.read().decode('utf-8')
+urlpath ='http://www.hazmatt.net/gaming/starcraft/zerg/units/'
+string = soup = BeautifulSoup(requests.get(urlpath).text)
 
-filelist = [out.split('\');">')[0] for out in string.split("javascript:PlaySound(\'")[1:-1]]
+filelist = [out.split('\');">')[0] for out in str(soup).split(
+            "javascript:PlaySound(\'")[1:-1]]
+
 print(filelist)
 
 my_path="../audio/"
 
 for filename in filelist:
+    testfile = urllib.URLopener()
     print 'http://www.hazmatt.net/gaming/starcraft/zerg/units/' + filename
-    remotefile = urlopen('http://www.hazmatt.net/gaming/starcraft/zerg/units/' + filename)
-    
-    save_name = filename.split("/")[-1]
-    localfile = open(my_path+save_name, 'w')
-    localfile.write(remotefile.read())
-    localfile.close()
-    remotefile.close()
+    testfile.retrieve(urlpath + filename, my_path+filename.split('/')[-1])
