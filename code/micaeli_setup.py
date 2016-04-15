d7829ab8c5b716b2c3af0612c3d3dd3373882f8a
cferko <cferko@domeyard.com>
2016-04-14 21:31:22 -0400

    REV: some micaeli fixes

--- code/micaeli_setup.py
+++ code/micaeli_setup.py
@@ -1,12 +1,23 @@
 import IPython
 from IPython.display import Audio as listen
+import numpy as np
 from scipy.io import wavfile
 
-%matplotlib inline
+from IPython import get_ipython
+ipython = get_ipython()
+ipython.magic("matplotlib inline")
+
+male_rate, male_audio = wavfile.read("../../audio/male_sample.wav")
+female_rate, female_audio = wavfile.read("../../audio/female_sample.wav")
+
+male_audio = np.mean(male_audio, axis=1)
+female_audio = np.mean(female_audio, axis=1)
+
 from matplotlib.pyplot import *
-import np.fft.rfft as raw_fft
+from numpy.fft import rfft as raw_fft
 
 def fft(audio_sample):
+    rfft_output = raw_fft(audio_sample)
     return [sqrt(i.real**2 + i.imag**2)/len(rfft_output) for i in rfft_output]
     
 def fft_freq(audio_sample):
@@ -14,3 +25,19 @@ def fft_freq(audio_sample):
     num_samples = len(audio_sample)
     return [(i*1.0/num_samples)*sample_rate for i in range(num_samples/2+1)]
     
+def get_male_example():
+    return male_audio
+    
+def get_female_example():
+    return female_audio
+
+def listen(audio):
+
+    if np.array_equal(audio, male_audio):
+        return IPython.display.Audio("../../audio/male_sample.wav")
+        
+    elif np.array_equal(audio, female_audio):
+        return IPython.display.Audio("../../audio/female_sample.wav")
+    
+    else:
+        return IPython.display.Audio(audio, rate = 22050)
